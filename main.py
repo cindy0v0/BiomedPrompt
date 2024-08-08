18008621c1fa69e8ed7fc4f527a01b76c2cb372d
cindy <xpshi@student.ubc.ca>
2024-08-08 12:09:46 -0700

    main

--- main.py
+++ main.py
@@ -17,21 +17,46 @@ from utils import load_imgs, compute_wasserstein_distance
 @click.option('--model', default='', type=str, help='')
 @click.option('--save_path', default='./', type=str, help='')
 def main(source_dataset_path, hashtag, model, save_path):
-    source_dataset_path = "/projects/ovcare/classification/Ali/Multi_mag_backbone/dataset/ocean_comp/features/test_sets/public/OV/finetuned/fold-1/20x/OV/PLIP/"
+    # source_dataset_path = "/projects/ovcare/classification/Ali/Multi_mag_backbone/dataset/ocean_comp/features/test_sets/public/OV/finetuned/fold-1/20x/OV/PLIP/"
+    source_dataset_path= "/projects/ovcare/classification/cshi/OCEAN/train_data_rep2/features/public/PLIP"
+
     hashtag = "OvarianPath"
-    classes = ["clear cell (CC)", "endometrioid (EC)", "high-grade serous (HGSC)", "low-grade serous (LGSC)", "mucinous (MC)", "Unusual ROI with in conclusive details"] # an unknown rare outlier subtype of
+    prompts = ["a patch image of ovarian carcinoma", 
+                "a H&E image of ovarian carcinoma with an unusual region of interest (ROI) and/or with inconclusive details",
+                "an endometrial H&E image with usual region of interest (ROI) and/or with conclusive details"
+                ]
+    # prompts = ["a H&E image of clear cell ovarian cancer carcinoma", 
+    #             "a H&E image of high grade serous ovarian carcinoma",
+    #             "a H&E image of low grade serous ovarian carcinoma",
+    #             "a H&E image of mucinous ovarian carcinoma",
+    #             "a H&E image of endometrioid ovarian carcinoma",
+    #             "a H&E image of ovarian carcinoma with an unusual region of interest (ROI) and/or with inconclusive details",
+    #             ]
+    classes = ["CC", "EC", "HGSC", "LGSC", "MC", "OOD", "Not OVC"] # an unknown rare outlier subtype of
+    classes = ['OVC Major Subtype', 'OOD OVC', 'Not OVC']
     model = text_encoder()
-    output = text_to_image_retrieval(source_dataset_path, hashtag, classes, model)
-    print(output['retrieved_image_labels'])
-    save_path="/projects/ovcare/classification/cshi/OCEAN_COMPETITION/BiomedPrompt/output/"
-    with open(save_path, 'wb') as f:
-        pickle.dump(output, f)
 
+    top_k = 48279 * 3
+    y_true, y_pred, y_score = text_to_image_retrieval(source_dataset_path, hashtag, prompts, model, top_k)
 
+    # metrics = evaluate_predictions(y_true, y_pred, y_score)
+
+    # print(f"Precision: {metrics['precision']:.3f}")
+    # print(f"Recall: {metrics['recall']:.3f}")
+    # print(f"F1-Score: {metrics['f1_score']:.3f}")
+    # print(f"ROC AUC: {metrics['roc_auc']:.3f}")
+    # print(f"Cohen's Kappa: {metrics['cohen_kappa']:.3f}")
+    # print(f"Confusion Matrix: \n{metrics['confusion_matrix']}")
+
+    # plot_confusion_matrix(metrics['confusion_matrix'], class_names)
+    # print(output['retrieved_image_labels'])
+    # save_path="/projects/ovcare/classification/cshi/OCEAN_COMPETITION/BiomedPrompt/output/"
+    # with open(save_path, 'wb') as f:
+    #     pickle.dump(output, f)
 
 
 # ref: https://github.com/DeepMicroscopy/Prototype-sampling/tree/main
-def text_to_image_retrieval(source_dataset_path, hashtag, classes, model):
+def text_to_image_retrieval(source_dataset_path, hashtag, prompts, model, top_k):
     """
     OpenPath data structure
     
@@ -51,63 +76,36 @@ def text_to_image_retrieval(source_dataset_path, hashtag, classes, model):
     set(df.loc[data["source"]=="PathLAION", "hashtag"]):
         {'----'}
     """
-    # image_embeddings = torch.tensor(np.load(os.path.join(source_dataset_path,
-    #                                                      "OpenPath_image_embeddings_normalized.npy")))  # (208414, 512)
-    # df = pd.read_csv(os.path.join(source_dataset_path, "dataframe_208K_rows.csv"),
-    #                  header=None,
-    #                  names=["id", "source", "hashtag", "web", "unknown1", "unknown2"],
-    #                  usecols=["id", "source", "hashtag", "web"])
-
-    # breast_df = df.loc[df["hashtag"] == hashtag]  # DateFrame (7513, 4)
-    # breast_image_embeddings = image_embeddings[breast_df["id"].values].float()  # (7513, 512)
-    
-    # features_size = {'Phikon': 768, 'CTransPath': 768, 'KimiaNet': 1024, 'PLIP': 512}
     
     img_embeddings, img_names, img_labels= load_imgs(source_dataset_path)
     
-    print("Retreiving images for ", hashtag)
-    # prompts = [f"an H&E image of {c} ovarian carcinoma" for c in classes]
-    prompts = ["a patch image of ovarian carcinoma", 
-                "a H&E image of ovarian carcinoma with an unusual region of interest (ROI) and/or with inconclusive details",
-                "a non-endometrial H&E image with an unusual region of interest (ROI) and/or with inconclusive details and is not ovarian carcinoma"]
-    outlier_class = 0
-    query_embeddings = model(prompts)  # (2, 512)
+    query_embeddings = model(prompts)  # (3, 512)
     query_embeddings /= torch.linalg.vector_norm(query_embeddings, dim=1, keepdim=True)
 
-    
     cos_sim = []
     for img_embedding in img_embeddings:
         sim = (100. * img_embedding @ query_embeddings.t()).softmax(dim=1)
         cos_sim.append(sim)
     cos_sim = torch.cat(cos_sim)
-    # cos_sim = (100. * img_embeddings @ query_embeddings.t()).softmax(dim=1)  # (7513, 2)
-    # wasserstein_distance = compute_wasserstein_distance(img_embeddings, query_embeddings) # (7513, 2)
-    # assign a label for the image (e.g., normal/tumor), return also the similarity to the class
-    values, indices = cos_sim.topk(1)  # (7513, 1), (7513, 1)
-    print(values.shape, indices.shape)
+    values, indices = cos_sim.topk(1)
+    print(values.shape, indices.shape, '\n')
+
     values, indices = values.detach().cpu().numpy().flatten(), indices.detach().cpu().numpy().flatten()
-    # select the 100 images with the highest feature similarity to the outlier tissue prompt
-    # k=1
-    # indices = np.argpartition(wasserstein_distance, -k, axis=-1)[:, -k:]
-    # values = np.take_along_axis(wasserstein_distance, indices, axis=-1)
-    most_similar = np.argsort(values[indices == outlier_class])[-48279:]
+    # select top k% images with the highest feature similarity to the class_index prompt
+    for class_index in range(len(prompts)):
+        most_similar = np.argsort(values[indices == class_index])[-top_k:]
+        filtered_img_names = [name for name, index in zip(img_names, indices) if index == class_index]
+        filtered_img_labels = [label for label, index in zip(img_labels, indices) if index == class_index]
+        retrieved_image_files = [filtered_img_names[i] for i in most_similar]
+        retrieved_image_labels = [filtered_img_labels[i] for i in most_similar]
 
-    # import pdb; pdb.set_trace()
+        count = Counter(retrieved_image_labels)
+        percentage = {key: (value / len(retrieved_image_labels)) * 100 for key, value in count.items()}
+        print(f"{prompts[class_index]}: {len(retrieved_image_labels)}")
+        print(percentage)
+        print(f"slide counts: {len(set(retrieved_image_files))}")
 
-    # retrieved_embedding = img_embeddings[indices == 1][most_similar]  # (100, 512)
-    # retrieved_image_files = breast_df.loc[indices == 1, "web"].values[most_similar]
-    filtered_img_names = [name for name, index in zip(img_names, indices) if index == outlier_class]
-    filtered_img_labels = [label for label, index in zip(img_labels, indices) if index == outlier_class]
-    retrieved_image_files = [filtered_img_names[i] for i in most_similar]
-    retrieved_image_labels = [filtered_img_labels[i] for i in most_similar]
-
-    count = Counter(retrieved_image_labels)
-    percentage = {key: (value / 48279) * 100 for key, value in count.items()}
-    print(percentage)
-
-    import pdb; pdb.set_trace()
-
-    return {'retrieved_embedding': retrieved_embedding, 'retrieved_image_files': retrieved_image_files, "retrieved_image_labels": retrieved_image_labels}
+    return None, None, None
 
 
 if __name__ == '__main__':
@@ -116,3 +114,32 @@ if __name__ == '__main__':
     multiprocessing.freeze_support()
     main()  # pylint: disable=no-value-for-parameter
     '''
+
+# # If you want to plot ROC curve
+# from sklearn.preprocessing import label_binarize
+# from sklearn.metrics import roc_curve, auc
+
+# y_true_bin = label_binarize(y_true, classes=np.unique(y_true))
+# n_classes = y_true_bin.shape[1]
+
+# fpr = dict()
+# tpr = dict()
+# roc_auc = dict()
+
+# for i in range(n_classes):
+#     fpr[i], tpr[i], _ = roc_curve(y_true_bin[:, i], y_score[:, i])
+#     roc_auc[i] = auc(fpr[i], tpr[i])
+
+# plt.figure(figsize=(10, 8))
+# for i, color in zip(range(n_classes), ['blue', 'red', 'green']):
+#     plt.plot(fpr[i], tpr[i], color=color, lw=2,
+#              label=f'ROC curve of class {class_names[i]} (area = {roc_auc[i]:.2f})')
+
+# plt.plot([0, 1], [0, 1], 'k--', lw=2)
+# plt.xlim([0.0, 1.0])
+# plt.ylim([0.0, 1.05])
+# plt.xlabel('False Positive Rate')
+# plt.ylabel('True Positive Rate')
+# plt.title('Receiver Operating Characteristic (ROC) Curve')
+# plt.legend(loc="lower right")
+# plt.show()
